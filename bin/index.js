3db028bdedc56da87f874adfc226b17e936d5cd9
Trackrise <trackriseproductions@gmail.com>
2021-05-20 08:04:44 -0700

    All done!

--- bin/index.js
+++ bin/index.js
@@ -6,21 +6,128 @@ const argv = yargs(hideBin(process.argv)).argv
 
 console.log("Playing a game of Roshambo against the computer.")
 
-if (argv.move === 'rock') {
+class userInput {
 
-    console.log("You chose rock!")
+    constructor(input, computer) {
 
-} else if (argv.move === 'paper') {
+        this.input = input
+        this.computer = computer
+    }
 
-    console.log("You chose paper!")
+    logInput() {
 
-} else if (argv.move === 'scissors') {
+        if (this.input === 'rock') {
 
-    console.log("You chose scissors!")
+            console.log("You chose rock!")
 
-} else {
+        } else if (this.input === 'paper') {
 
-    console.log("Your move is invalid, or no move provided.")
+            console.log("You chose paper!")
+
+        } else if (this.input === 'scissors') {
+
+            console.log("You chose scissors!")
+
+        } else {
+
+            console.log("Your move is invalid, or no move provided.")
+
+            throw 'Unfair game!'
+        }
+
+        return this.computer.getRandomMove()
+    }
 }
 
-console.log("Computer logic not yet written.")
+class computerLogic {
+
+    getRandomMove() {
+
+        let rand = Math.floor(Math.random() * 3)
+
+        if (rand === 0){
+
+            console.log('The computer chose rock!')
+
+            return 'rock'
+
+        } else if (rand === 1){
+
+            console.log('The computer chose paper!')
+
+            return 'paper'
+
+        } else if (rand === 2){
+
+            console.log('The computer chose scissors!')
+
+            return 'scissors'
+        }
+    }
+}
+
+class gameLogic {
+
+    constructor(user, computer) {
+
+        this.user = user
+        this.computer = computer
+    }
+
+    determineWinner() {
+
+        if (this.user === 'rock') {
+
+            if (this.computer === 'paper') {
+
+                console.log('Computer wins!')
+
+
+            } else if (this.computer === 'scissors') {
+
+                console.log('Player wins!')
+
+            } else {
+
+                console.log('Tie game!')
+            }
+
+        } else if (this.user === 'paper') {
+
+            if (this.computer === 'scissors') {
+
+                console.log('Computer wins!')
+
+            } else if (this.computer === 'rock') {
+
+                console.log('Player wins!')
+
+            } else {
+
+                console.log('Tie game!')
+
+            }
+
+        } else if (this.user === 'scissors') {
+
+            if (this.computer === 'rock') {
+
+                console.log('Computer wins!')
+
+            } else if (this.computer === 'paper') {
+
+                console.log('Player wins!')
+
+            } else {
+
+                console.log('Tie game!')
+            }
+        }
+    }
+}
+
+let computer1 = new computerLogic()
+let user1 = new userInput(argv.move, computer1)
+let gameController = new gameLogic(argv.move, user1.logInput())
+
+gameController.determineWinner()
