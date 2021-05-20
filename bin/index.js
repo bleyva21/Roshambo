5e201ab431c48e06f5535b8832fc8efdc5cbe6e7
Trackrise <trackriseproductions@gmail.com>
2021-05-19 20:18:49 -0700

    Initial setup for Roshambo project. Testing yargs.

---
+++ bin/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
+console.log("Playing a game of Roshambo against the computer.")
+
+if (argv.move === 'rock') {
+
+    console.log("You chose rock!")
+
+} else if (argv.move === 'paper') {
+
+    console.log("You chose paper!")
+
+} else if (argv.move === 'scissors') {
+
+    console.log("You chose scissors!")
+
+} else {
+
+    console.log("Your move is invalid.")
+}
